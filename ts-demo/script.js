330a5ec0cbc24dc4a24e87a4266c6406560f851b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-06 17:43:35 +0530

    js basics

---
+++ ts-demo/script.js
@@ -0,0 +1,20 @@
+
+// npm install --location=global typescript 
+// npm install typescript 
+// tsc -v
+
+let salary = 10.5;
+console.log(salary);
+
+salary = 12.75;
+console.log(salary);
+
+salary = 'abc';
+console.log(salary);
+
+// type safety 
+
+// write ts code -> transpile -> js code -> run the code 
+
+
+
