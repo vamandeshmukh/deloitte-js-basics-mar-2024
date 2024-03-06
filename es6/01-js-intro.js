53009a5207c59f1eb8a1a771c88c2e59f6aee308
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-06 11:30:50 +0530

    js basics

--- es6/01-js-intro.js
+++ es6/01-js-intro.js
@@ -1,23 +1,59 @@
 
-console.log('JS intro');
+// console.log('JS intro');
 
-num1 = 10; // do not use 
-var num2 = 20; // do not use 
-const num3 = 30; // use this as default 
-let num4 = 40; // use this as well 
+// num1 = 10; // do not use 
+// var num2 = 20; // do not use 
+// const num3 = 30; // use this as default 
+// let num4 = 40; // use this as well 
 
-// int num5 = 50;  // java 
+// // int num5 = 50;  // java 
 
-// console.log(num3);
-// // num3 = 35; // TypeError: Assignment to constant variable.
-// console.log(num3);
+// // console.log(num3);
+// // // num3 = 35; // TypeError: Assignment to constant variable.
+// // console.log(num3);
 
-console.log(num4);
-num4 = 45; // this works
-console.log(num4);
-num4 = 'abc'; 
-console.log(num4);
-num4 = false; 
-console.log(num4);
+// console.log(num4);
+// num4 = 45; // this works
+// console.log(num4);
+// num4 = 'abc';
+// console.log(num4);
+// num4 = false;
+// console.log(num4);
+
+
+// // let firstName = "Sonu";
+// // let firstName = 'Sonu';
+// let firstName = `Sonu`;
+
+// // let salary = 10;
+// let salary = 10.5;
+
+
+// if (firstName == 'Sonu')
+//     console.log(firstName);
+// else
+//     console.log('Monu');
+
+// // array 
+// const myArray = [10, 20.25, false, 'Abc', null];
+
+// object 
+
+const employee = {
+    eid: 101,
+    firstName: 'Sonu',
+    lastName: 'Singh',
+    salary: 10.5,
+    isMarried: false,
+    phones: [9876, 9809],
+    address: { city: 'Mumbai', pin: 400001 }
+};
+
+// console.log(employee);
+console.log(employee.firstName);
+
+
+
+
 
 
