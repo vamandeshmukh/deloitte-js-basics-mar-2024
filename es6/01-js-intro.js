9444230c1a0e6b14ed9797f2f8e9088a42866512
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-06 11:02:48 +0530

    js basics

--- es6/01-js-intro.js
+++ es6/01-js-intro.js
@@ -6,15 +6,18 @@ var num2 = 20; // do not use
 const num3 = 30; // use this as default 
 let num4 = 40; // use this as well 
 
-// java 
-// int num5 = 50;
+// int num5 = 50;  // java 
 
-
-console.log(num3);
-// num3 = 35; // TypeError: Assignment to constant variable.
-console.log(num3);
+// console.log(num3);
+// // num3 = 35; // TypeError: Assignment to constant variable.
+// console.log(num3);
 
 console.log(num4);
 num4 = 45; // this works
 console.log(num4);
+num4 = 'abc'; 
+console.log(num4);
+num4 = false; 
+console.log(num4);
+
 
