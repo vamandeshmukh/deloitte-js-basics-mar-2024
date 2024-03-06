908df4695867544b9f2a37eccaf1ceb90afa24ec
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-06 17:47:37 +0530

    js basics

--- ts-demo/app.ts
+++ ts-demo/app.ts
@@ -1,2 +1,23 @@
 // app.ts
 
+// npm install --location=global typescript 
+// npm install typescript 
+// tsc -v
+
+// TypeScript provides type safety to JavaScript  
+// write ts code -> transpile -> js code -> run the code 
+
+// process 
+// write code in app.ts 
+// tsc app.ts 
+// app.ts will be generated 
+// node app.js 
+
+let salary: number = 10.5;
+console.log(salary);
+
+salary = 12.75;
+console.log(salary);
+
+salary = 'abc';
+console.log(salary);
