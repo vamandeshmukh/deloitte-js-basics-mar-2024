17c8b7de8192b0938a6690cd8b0b01ff87814a99
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-07 10:26:21 +0530

    ts basics

--- ts-demo/dist/app.js
+++ ts-demo/dist/app.js
@@ -1,52 +1,29 @@
+"use strict";
 // app.ts
-
 // npm install --location=global typescript 
 // npm install typescript 
 // tsc -v
-
 // TypeScript provides type safety to JavaScript  
 // write ts code -> transpile -> js code -> run the code 
-
 // process 
 // write code in app.ts 
 // tsc app.ts 
 // app.js will be generated 
 // node app.js 
 // -----------
-
 // ts config 
-
 // npx tsc --init
 // OR 
 // tsc --init
-// ts config will be generated 
-// henceforth, 
-// tsc 
-// dist folder will be generated and 
-// in that app.js will be generated 
-// node dist/app.js 
-
-
-
-
-
 // let salary: number = 10.5;
 // console.log(salary);
-
 // salary = 12.75;
 // console.log(salary);
-
 // // salary = 'abc';
 // console.log(salary);
-
-
 // let salary: number = 20;
-
-const addNums = (a: number, b: number): number => {
+const addNums = (a, b) => {
     return a + b;
     // return 'abc'; // error 
 };
-
 console.log(addNums(10, 20));
-
-
